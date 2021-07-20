f70f1a309f6b9dc74f2885201bd01f66a61fa1ba
Rachael Davis <80214032+themetalsaint@users.noreply.github.com>
2021-07-20 18:53:58 -0400

    fixing product routes

--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -4,9 +4,9 @@ const { Product, Category, Tag, ProductTag } = require('../../models');
 // The `/api/products` endpoint
 
 // get all products
-router.get('/', (req, res) => {
+router.get('/', async (req, res) => {
   // find all products
-  // be sure to include its associated Category and Tag data - DONE
+  // be sure to include its associated Category and Tag data
   try {
     const productsData = await Product.findAll({
       include: [{ model: Category}, { model: Tag, through: ProductTag}]
@@ -18,9 +18,9 @@ router.get('/', (req, res) => {
 });
 
 // get one product
-router.get('/:id', (req, res) => {
+router.get('/:id', async (req, res) => {
   // find a single product by its `id`
-  // be sure to include its associated Category and Tag data - DONE
+  // be sure to include its associated Category and Tag data
   try {
     const productData = await Product.findByPk(req.params.id, {
       include: [{ model: Category}, { model: Tag, through: ProductTag}]
@@ -40,8 +40,8 @@ router.post('/', (req, res) => {
       stock: 3,
       tagIds: [1, 2, 3, 4]
     }
-  */
-  Product.create(req.body)
+    */
+ Product.create(req.body)
     .then((product) => {
       // if there's product tags, we need to create pairings to bulk create in the ProductTag model
       if (req.body.tagIds.length) {
@@ -100,13 +100,12 @@ router.put('/:id', (req, res) => {
     })
     .then((updatedProductTags) => res.json(updatedProductTags))
     .catch((err) => {
-      // console.log(err);
       res.status(400).json(err);
     });
 });
 
-router.delete('/:id', (req, res) => {
-  // delete one product by its `id` value - DONE
+router.delete('/:id', async (req, res) => {
+  // delete one product by its `id` valuev - DONE
   try {
     const productData = await Product.destroy({
       where: {
@@ -114,7 +113,7 @@ router.delete('/:id', (req, res) => {
       }
     });
     if (!productData) {
-      res.status(404).json({ message: 'No product found with this id! Please try again!' });
+      res.status(404).json({ message: 'No product found with this id!' });
       return;
     }
     res.status(200).json(productData);
